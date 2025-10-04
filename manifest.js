a0e1ee61ce7215b6e545378a00a4e9af17016347
Forhad Khan <fk.nafi11@gmail.com>
2025-10-04 14:27:00 +0600

    pwa install

---
+++ manifest.js
@@ -0,0 +1,41 @@
+export const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssests: ["favicon.svg", "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "React-vite-app",
+    short_name: "react-vite-app",
+    description: "I am a simple vite app",
+    icons: [
+      {
+        src: "/favicon.svg",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/favicon.svg",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/favicon.svg",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/favicon.svg",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugIn } from './manifest';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+  plugins: [react(), tailwindcss(), VitePWA(manifestForPlugIn)],
+});
