6d0f9ecb0d8194eb988370babd731965c887fe77
Forhad Khan <fk.nafi11@gmail.com>
2025-10-05 12:43:44 +0600

    done

--- src/sw.js
+++ src/sw.js
@@ -1,21 +1,12 @@
-/* eslint-disable no-undef */
 import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate } from "workbox-strategies";
+
 precacheAndRoute(self.__WB_MANIFEST);
 
-// 🔔 Push Notifications
-self.addEventListener("push", (event) => {
-  const data = event.data ? event.data.json() : {};
-  const title = data.title || "🔔 Notification";
-  const options = {
-    body: data.body || "You got a message",
-    icon: "/icon-192.png",
-    badge: "/icon-192.png",
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
-});
-
-// 🖱️ Notification click
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-  event.waitUntil(clients.openWindow("/"));
-});
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new StaleWhileRevalidate({
+    cacheName: "images",
+  })
+);
