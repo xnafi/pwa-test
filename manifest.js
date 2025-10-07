32e2009b9f905df3ffba63ed5fab16849b4f7b88
Forhad Khan <fk.nafi11@gmail.com>
2025-10-07 11:19:22 +0600

    updated

--- manifest.js
+++ manifest.js
@@ -3,37 +3,29 @@ export const manifestForPlugIn = {
     name: "React-vite-app",
     short_name: "react-vite-app",
     description: "I am a simple vite app",
+    icons: [
+      {
+        src: "/apple.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
     theme_color: "#171717",
     background_color: "#f0e7db",
     display: "standalone",
     scope: "/",
     start_url: "/",
     orientation: "portrait",
-    icons: [
-      {
-        src: "/favicon.svg",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/favicon.svg",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "favicon",
-      },
-      {
-        src: "/apple.png",
-        sizes: "180x180",
-        type: "image/png",
-        purpose: "apple touch icon",
-      },
-      {
-        src: "/android.png",
-        sizes: "512x512",
-        type: "image/png",
-        purpose: "any maskable",
-      },
-    ],
   },
 };
